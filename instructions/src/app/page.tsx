d093c5f4c4646748a86e6e66b88c38edfb83a65c
usany <ckd_qja@naver.com>
2026-01-07 17:53:04 +0900

    cern

--- instructions/src/app/page.tsx
+++ instructions/src/app/page.tsx
@@ -1,12 +1,16 @@
 // import { Box, Button, Container, Typography, useMediaQuery, useTheme } from '@mui/material';
 // import styled from 'styled-components';
 import Explanation from './explanation';
+import { headers } from 'next/headers';
 
+export default async function HomePage() {
+  const headersList = await headers();
+  const pathname = headersList.get('x-pathname') || '/';
+  
+  console.log('Current pathname:', pathname);
 
-export default function HomePage() {
   // const theme = useTheme();
   // const isMobile = useMediaQuery(theme.breakpoints.down('md'));
-
   return (
     <div>
       {/* <div>Posts Documentation</div> */}
