91ee425cefb2df812c70185b1704eb2796cc0a2c
usany <ckd_qja@naver.com>
2026-01-08 21:29:44 +0900

    cern

---
+++ instructions/src/app/registers/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import styles from './register.module.css';
+import fs from 'fs';
+import path from 'path';
+import Comments from '@/components/Comments';
+import SideNav from '@/components/SideNav';
+import PageLayout from '@/components/pageLayout';
+
+export default function RegisterPage() {
+  const registerPath = path.join(process.cwd(), 'src/content/register.mdx');
+  const registerContent = fs.readFileSync(registerPath, 'utf8');
+
+  return (
+    <PageLayout file={registerContent} pageId="register" />
+  );
+}
