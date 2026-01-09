3f1cc8ddfd8bf693e415e9b997f7205fedaaeb45
usany <ckd_qja@naver.com>
2026-01-09 20:38:41 +0900

    cern

--- instructions/src/app/docs/page.tsx
+++ instructions/src/app/docs/page.tsx
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-import styles from './docs.module.css';
-import fs from 'fs';
-import path from 'path';
-import Comments from '@/components/Comments';
-import SideNav from '@/components/SideNav';
+'use client'
 import PageLayout from '@/components/pageLayout';
+import docs from '@/content/docs.mdx';
 
 export default function DocsPage() {
-  const docsPath = path.join(process.cwd(), 'src/content/docs.md');
-  const docsContent = fs.readFileSync(docsPath, 'utf8');
 
   return (
-    <PageLayout file={docsContent} pageId="docs" />
+    <PageLayout file={docs} pageId="docs" />
   );
 }
