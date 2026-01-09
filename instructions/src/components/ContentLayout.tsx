d9060cfb8236cfed192a784e6af8cbff3fe1e84b
charlie <ckd_qja@naver.com>
2026-01-10 02:05:03 +0900

    cern

--- instructions/src/components/ContentLayout.tsx
+++ instructions/src/components/ContentLayout.tsx
@@ -4,22 +4,19 @@ import BottomNavigation from '@mui/material/BottomNavigation'
 import BottomNavigationAction from '@mui/material/BottomNavigationAction'
 import { Pencil, Presentation, Umbrella } from 'lucide-react'
 import { ComponentType } from 'react'
-// import { useLocation, useNavigate } from 'react-router-dom'
 
 const components = {
-  h1: () => <h1 />, 
-  h2: () => <h2 />, 
-  p: () => <p />, 
+  h1: () => <h1 />,
+  h2: () => <h2 />,
+  p: () => <p />,
   Button: (props: any) => <Button {...props} />,
   BottomNavigation: (props: any) => <BottomNavigation {...props} />,
   BottomNavigationAction: (props: any) => <BottomNavigationAction {...props} />,
 }
-function ContentLayout({content}: {content: ComponentType<any>}) {
+function ContentLayout({ content }: { content: ComponentType<any> }) {
   const ContentComponent = content
   return (
-    <MDXProvider components={components}>
-      <ContentComponent />
-    </MDXProvider>
+    <ContentComponent />
   )
 }
 
