0cc3b688aae57cb415b466b03c5637b34ec79a90
charlie <ckd_qja@naver.com>
2026-01-10 03:06:19 +0900

    cern

--- instructions/src/app/register/page.tsx
+++ instructions/src/app/register/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 import PageLayout from '@app/[slug]/components/pageLayout';
-import register from '@/content/register.mdx';
+import register from '@app/content/register.mdx';
 export default function RegisterPage() {
   return (
     <PageLayout file={register} pageId="register" />
   );
 }
+q
