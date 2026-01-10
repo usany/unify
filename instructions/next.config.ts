72da7ff43056204297266ce90959f6db499ab13b
charlie <ckd_qja@naver.com>
2026-01-11 03:17:50 +0900

    cern

--- instructions/next.config.ts
+++ instructions/next.config.ts
@@ -5,9 +5,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  // compiler: {
-  //   styledComponents: true,
-  // },
 };
 
 const withMDX = createMDX();
