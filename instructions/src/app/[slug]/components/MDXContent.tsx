a5e6be6f84681481e1361d53bbc751b33c4c75ca
charlie <ckd_qja@naver.com>
2026-01-12 21:26:55 +0900

    cern

--- instructions/src/app/[slug]/components/MDXContent.tsx
+++ instructions/src/app/[slug]/components/MDXContent.tsx
@@ -6,10 +6,10 @@ import { useLanguage } from '@/context/LanguageContext';
 const MDXContent = ({ slug }: { slug: string }) => {
     const { language } = useLanguage();
     const Content = dynamic(() => {
-        if (language === 'en') {
-            return import(`@contents/${slug}En.mdx`);
+        if (language === 'ko') {
+            return import(`@contents/${slug}.mdx`);
         }
-        return import(`@contents/${slug}.mdx`);
+        return import(`@contents/${slug}En.mdx`);
     }, {
         loading: () => <div>Loading...</div>,
     });
