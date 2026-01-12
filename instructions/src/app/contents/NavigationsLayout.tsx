a136ec50156c5adc13461e0d785ef065c1ca8bfa
charlie <ckd_qja@naver.com>
2026-01-12 20:09:07 +0900

    cern

--- instructions/src/app/contents/NavigationsLayout.tsx
+++ instructions/src/app/contents/NavigationsLayout.tsx
@@ -1,24 +1,32 @@
 import BottomNavigation from '@mui/material/BottomNavigation'
 import BottomNavigationAction from '@mui/material/BottomNavigationAction'
 import { Pencil, Presentation, Umbrella } from 'lucide-react'
+import { usePathname, useRouter } from 'next/navigation'
 
-function NavigationsLayout({ onChange, bottomNavigation }: { onChange: (event: React.SyntheticEvent, value: number) => void, bottomNavigation: number, tabs: boolean }) {
+function NavigationsLayout() {
+  const pathname = usePathname()
+  const router = useRouter()
   return (
     <BottomNavigation
       showLabels
-      value={bottomNavigation}
-      onChange={onChange}
+      value={pathname}
+      onChange={(event, newValue) => {
+        router.push(newValue)
+      }}
     >
       <BottomNavigationAction
         label="Register"
+        value="/register"
         icon={<Pencil />}
       />
       <BottomNavigationAction
         label="Login"
+        value="/login"
         icon={<Umbrella />}
       />
       <BottomNavigationAction
         label="Board"
+        value="/board"
         icon={<Presentation />}
       />
     </BottomNavigation>
