1afe377f3103112f908833e74824cf55c42ba55a
usany <ckd_qja@naver.com>
2026-01-14 21:57:55 +0900

    cern

--- src/app/contents/CardViewTime.tsx
+++ src/app/contents/CardViewTime.tsx
@@ -11,15 +11,15 @@ const CardViewTime = ({ message }) => {
       <div className="flex flex-col justify-center">
         <div className="flex">
           {language === 'en' && <div className="flex justify-start w-[40px]">From</div>}
-          {message.text.clock?.year}.{message.text.clock?.month}.
+          {/* {message.text.clock?.year}.{message.text.clock?.month}.
           {message.text.clock?.day} {message.text.clock?.hour}:
-          {message.text.clock?.minute} {languages === 'ko' && ' 부터'}
+          {message.text.clock?.minute} {language === 'ko' && ' 부터'} */}
         </div>
         <div className="flex">
           {language === 'en' && <div className="flex justify-start w-[40px]">To</div>}
-          {message.text.clocker?.year}.{message.text.clocker?.month}.
+          {/* {message.text.clocker?.year}.{message.text.clocker?.month}.
           {message.text.clock?.day} {message.text.clocker?.hour}:
-          {message.text.clocker?.minute} {languages === 'ko' && ' 까지'}
+          {message.text.clocker?.minute} {language === 'ko' && ' 까지'} */}
         </div>
       </div>
     </div>
