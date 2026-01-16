82b1037eb44b01ca633c4f3624836472978e646a
usany <ckd_qja@naver.com>
2026-01-16 23:41:09 +0900

    cern

--- src/app/contents/Collection.tsx
+++ src/app/contents/Collection.tsx
@@ -1,33 +1,5 @@
-import { GoogleGenAI } from '@google/genai'
-import { Button } from '@mui/material'
-import { decode } from 'base64-arraybuffer'
-import { arrayRemove, arrayUnion, collection, deleteDoc, doc, getDocs, setDoc, updateDoc } from 'firebase/firestore'
 import { Ban, BanIcon, Delete, Film, PlusCircle, ThumbsDown, ThumbsUp } from 'lucide-react'
-import { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import supabase from 'src/baseApi/base'
-import { dbservice } from 'src/baseApi/serverbase'
-import {
-  MorphingDialog,
-  MorphingDialogContainer,
-  MorphingDialogContent,
-  MorphingDialogTrigger,
-} from 'src/components/ui/morphing-dialog'
-import useSelectors from 'src/hooks/useSelectors'
-import useTexts from 'src/hooks/useTexts'
-import LottieOnce from 'src/lottiesAnimation/LottieOnce'
-import LottieProcess from 'src/lottiesAnimation/LottieProcess'
-import { changeBottomNavigation } from 'src/stateSlices/bottomNavigationSlice'
-import Avatars from '../core/Avatars'
-import PageTitle from '../core/pageTitle/PageTitle'
-import Popups from '../core/Popups'
-import useCardsBackground from 'src/hooks/useCardsBackground'
-import FileOpenIcon from '@mui/icons-material/FileOpen';
 import { AnimatedGroup } from 'src/components/motion-primitives/animated-group'
-import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
-import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
-import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import { useLanguage } from '@/context/LanguageContext'
 
 function Collection() {
@@ -47,10 +19,6 @@ function Collection() {
               <img
                 src={element.defaultProfile}
                 className="w-[80px] h-[80px]"
-                // onClick={() => {
-                //   navigate(`/collection?card=${element.uid}`, {replace: true})
-                //   setConnectedUsers(element.connectedUsers)
-                // }}
               />
             )
           })}
