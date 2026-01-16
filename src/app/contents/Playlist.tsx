980adcad3a146ba0a2bd22e3cbd645bc54bf10dc
usany <ckd_qja@naver.com>
2026-01-16 21:00:42 +0900

    cern

--- src/app/contents/Playlist.tsx
+++ src/app/contents/Playlist.tsx
@@ -4,12 +4,12 @@ const Playlist = () => {
   const { theme } = useTheme();
   return (
     <div
-      className={`fixed bottom-0 left-0 z-[120] flex justify-start transition-all ${open ? 'opacity-100 pointer-events-auto duration-250' : 'opacity-0 pointer-events-none duration-0'
+      className={`fixed bottom-0 left-0 z-[120] flex justify-start transition-all opacity-100 pointer-events-auto duration-250'
         }`}
     >
       <div className="flex w-[350px] justify-center">
         <iframe
-          src={import.meta.env.VITE_SPOTIFY_URL + `${theme !== 'light' ? '&theme=0' : ''}`}
+          src={'https://open.spotify.com/embed/playlist/41clCj2piQBL3BSEFQN9J3?utm_source=generator' + `${theme !== 'light' ? '&theme=0' : ''}`}
           width="90%"
           height="200"
           allow="autoplay; clipboard-write; fullscreen; picture-in-picture"
