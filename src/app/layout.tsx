440ddd178b998871de0a6dd475a4024e80bccc01
usany <ckd_qja@naver.com>
2026-01-16 21:37:02 +0900

    cern

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { LanguageProvider } from '@/app/context/LanguageContext';
 import { ThemeProvider } from '@/app/context/ThemeContext';
 import MuiThemeProvider from '@/components/MuiThemeProvider';
+import Script from 'next/script';
 import type { Metadata } from 'next';
 import { cookies } from 'next/headers';
 import './globals.css';
@@ -32,6 +33,14 @@ export default async function RootLayout({ children }: { children: React.ReactNo
             </ThemeProvider>
           </LanguageProvider>
         </AppRouterCacheProvider>
+        
+        <Script 
+          src="https://open.spotify.com/embed/iframe-api/v1" 
+          async 
+        />
+        <Script 
+          src="https://oapi.map.naver.com/openapi/v3/maps.js?ncpKeyId=yi6uxw634q&language=en"
+        />
       </body>
     </html>
   );
