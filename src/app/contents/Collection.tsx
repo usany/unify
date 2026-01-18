6902add9036690ef03b989e24517c6a96afe0635
usany <ckd_qja@naver.com>
2026-01-18 21:28:26 +0900

    cern

--- src/app/contents/Collection.tsx
+++ src/app/contents/Collection.tsx
@@ -1,28 +1,32 @@
 import { Ban, BanIcon, Delete, Film, PlusCircle, ThumbsDown, ThumbsUp } from 'lucide-react'
-import { AnimatedGroup } from 'src/components/motion-primitives/animated-group'
+import { AnimatedGroup } from '../../components/motion-primitives/animated-group'
 import { useLanguage } from '@/context/LanguageContext'
-
+import staticImg from '../../../public/applicationImage.jpg'
+import { Button } from '@mui/material'
+import Image from 'next/image'
 function Collection() {
   const { language } = useLanguage()
-  const images = []
+  const images = [staticImg]
   return (
     <div>
-      <button
+      <Button
+        variant='outlined'
         className="flex gap-5 justify-center"
       >
         <PlusCircle />
         {language === 'en' ? 'Register' : '등록'}
-      </button>
+      </Button>
       {images.length > 0 && 
         <AnimatedGroup className="grid grid-cols-[repeat(auto-fit,minmax(80px,1fr))] col-span-full p-5">
-          {images.map((element, index) => {
+          {/* {images.map((element, index) => {
             return (
-              <img
-                src={element.defaultProfile}
-                className="w-[80px] h-[80px]"
-              />
             )
-          })}
+          })} */}
+          <Image
+            src={staticImg}
+            className="w-[80px] h-[80px]"
+            alt='khusan'
+          />
         </AnimatedGroup>
       }
     </div>
