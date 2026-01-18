c8a8d1437eee376684083483fc7ad01e12d0f28f
usany <ckd_qja@naver.com>
2026-01-18 22:37:52 +0900

    cern

---
+++ src/app/contents/SearchBar.tsx
@@ -0,0 +1,11 @@
+import TextField from '@mui/material/TextField';
+
+function SearchBar() {
+  return (
+    <div className='px-5 flex justify-center'>
+      <TextField sx={{ width: '1000px', borderRadius: '5px' }} />
+    </div>
+  )
+}
+
+export default SearchBar
