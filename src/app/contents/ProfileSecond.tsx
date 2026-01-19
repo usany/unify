ecbb8810221382d478b3e0c6b914855f58573309
charlie <ckd_qja@naver.com>
2026-01-19 20:08:26 +0900

    cern

--- src/app/contents/ProfileSecond.tsx
+++ src/app/contents/ProfileSecond.tsx
@@ -15,27 +15,30 @@ const campuses = {
 }
 const ProfileSecond = () => {
   const {language} = useLanguage()
+  
   return (
-    <div className='flex items-center'>
-      <Select defaultValue={campuses.en[campuses.en.indexOf('Seoul Campus')]}>
-        <SelectTrigger
-          className="w-52 bg-light-1 dark:bg-dark-1"
-        >
-          <SelectValue />
-        </SelectTrigger>
-        <SelectContent className="bg-light-1 dark:bg-dark-1">
-          <SelectGroup>
-            {campuses[language].map((value, index) => {
-              return (
-                <SelectItem key={index} value={campuses.en[index]}>
-                  {value}
-                </SelectItem>
-              )
-            })}
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <Button>{language === 'ko' ? '저장' : 'Save'}</Button>
+    <div className={`flex justify-center gap-1`}>
+      <div className='flex items-center'>
+        <Select defaultValue={campuses.en[campuses.en.indexOf('Seoul Campus')]}>
+          <SelectTrigger
+            className="w-52 bg-light-1 dark:bg-dark-1"
+          >
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent className="bg-light-1 dark:bg-dark-1">
+            <SelectGroup>
+              {campuses[language].map((value, index) => {
+                return (
+                  <SelectItem key={index} value={campuses.en[index]}>
+                    {value}
+                  </SelectItem>
+                )
+              })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        <Button>{language === 'ko' ? '저장' : 'Save'}</Button>
+      </div>
     </div>
   )
 }
