93037ae7be41d675955a8c6e2113729ac665ec6a
charlie <ckd_qja@naver.com>
2026-01-19 19:18:51 +0900

    cern

--- src/app/contents/CardsViews.tsx
+++ src/app/contents/CardsViews.tsx
@@ -1,13 +1,7 @@
 import Tilt from 'react-parallax-tilt'
 import CardView from './CardView'
-import { DocumentData } from 'firebase/firestore'
 import getShadowColor from './getShadowColor'
-interface Props {
-  message: DocumentData
-  onPulse?: boolean
-  onTransfer?: boolean
-}
-const CardsViews = ({ message, onTransfer }: Props) => {
+const CardsViews = ({ message, onTransfer }: { message: any, onTransfer: boolean }) => {
   const id = Date.now().toString()
   const shadowColor = getShadowColor(id)
   return (
