95e539d374a35e40180226360f563b6218e27272
usany <ckd_qja@naver.com>
2026-01-20 21:34:52 +0900

    cern

--- src/app/contents/CardView.tsx
+++ src/app/contents/CardView.tsx
@@ -5,52 +5,49 @@ import CardMedia from '@mui/material/CardMedia'
 import CardViewLocation from './CardViewLocation'
 import CardViewTime from './CardViewTime'
 import CardViewTop from './CardViewTop'
-// import CardViewTransfer from './CardViewTransfer'
-// import { buildingsObj, staticArray } from 'src/pages/add/locationsBuildings'
+import Tilt from 'react-parallax-tilt'
+import getShadowColor from './getShadowColor'
 
-const CardView = ({ onTransfer, message, shadowColor }: { onTransfer: boolean, message: any, shadowColor: string }) => {
-  // const buildingsObject = {
-  //   input: buildingsObj.input,
-  //   ...buildingsObj.se,
-  //   ...buildingsObj.gu,
-  //   ...buildingsObj.gw
-  // }
-  // const key = Object.keys(buildingsObject).find((key) => buildingsObject[key].ko.name === locationOne)
-  // const staticImg = buildingsObject[key]?.image
-  // const staticImg = staticArray[message.text.count] || staticArray['building']
+const CardView = () => {
+  const id = Date.now().toString()
+  const shadowColor = getShadowColor(id)
+
   return (
-    <div className="flex flex-col gap-5">
-      {/* {onTransfer && <CardViewTransfer />} */}
-      <Card
-        className='colorTwo'
-        sx={{
-          width: 200 * 0.9,
-          height: 280 * 0.9,
-          boxShadow: `1.5px 1.5px 1.5px 1.5px ${shadowColor}`,
-          // bgcolor: colorTwo,
-        }}
-      >
-        <CardContent
-          sx={{ padding: '5px', bgcolor: alpha('#000000', 0.5) }}
+    <Tilt>
+
+      <div className="flex flex-col gap-5">
+        {/* {onTransfer && <CardViewTransfer />} */}
+        <Card
+          className='colorTwo'
+          sx={{
+            width: 200 * 0.9,
+            height: 280 * 0.9,
+            boxShadow: `1.5px 1.5px 1.5px 1.5px ${shadowColor}`,
+            // bgcolor: colorTwo,
+          }}
         >
-          <CardViewTop message={message} />
-          <div className="flex justify-center pt-1">
-            <CardMedia
-              sx={{
-                width: 200 * 0.9,
-                height: 141 * 0.9,
-                borderRadius: '10px'
-              }}
-              image={''}
-            />
-          </div>
-          <div className="flex flex-col pt-1 gap-1 text-xs">
-            <CardViewLocation message={message} />
-            <CardViewTime message={message} />
-          </div>
-        </CardContent>
-      </Card>
-    </div>
+          <CardContent
+            sx={{ padding: '5px', bgcolor: alpha('#000000', 0.5) }}
+          >
+            <CardViewTop message={message} />
+            <div className="flex justify-center pt-1">
+              <CardMedia
+                sx={{
+                  width: 200 * 0.9,
+                  height: 141 * 0.9,
+                  borderRadius: '10px'
+                }}
+                image={''}
+              />
+            </div>
+            <div className="flex flex-col pt-1 gap-1 text-xs">
+              <CardViewLocation message={message} />
+              <CardViewTime message={message} />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </Tilt>
   )
 }
 export default CardView
