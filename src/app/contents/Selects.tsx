8f60964068db790ee05b7ced6dbf9994929af5c1
usany <ckd_qja@naver.com>
2026-01-21 21:16:29 +0900

    cern

--- src/app/contents/Selects.tsx
+++ src/app/contents/Selects.tsx
@@ -8,6 +8,7 @@ import useSelectors from 'src/hooks/useSelectors';
 import locationsBuildings, { buildingsObj, locationsBuildingsArray, locationsCollectionLetters } from "./locationsBuildings";
 // import locationsCollection from "./locationsCollection";
 import useTexts from "src/hooks/useTexts";
+import { useLanguage } from '@/context/LanguageContext';
 
 const settingSeats = (number) => {
   return Array(number)
@@ -87,10 +88,10 @@ function Selects({
   changeLocationInput,
 }: Props) {
   const matches = useMediaQuery("(min-width:990px)");
-  const languages = useSelectors((state) => state.languages.value)
+  const { language } = useLanguage()
   const locationOne = locationState?.locationOne
   const key = locationOne ? Object.keys(locationsCollectionLetters).find((key) => locationsCollectionLetters[key] === locationState.locationOne) : ''
-  const details = (locationOne && locationOne !== '직접 입력') ? buildingsObj[key.slice(0, 2)][key][languages].details : {}
+  const details = (locationOne && locationOne !== '직접 입력') ? buildingsObj[key.slice(0, 2)][key][language].details : {}
   const koDetails = (locationOne && locationOne !== '직접 입력') ? buildingsObj[key.slice(0, 2)][key]['ko'].details : {}
   const menuItems = (locationOne && locationOne !== '직접 입력') ? settingLocations(details, koDetails) : null
   const {selfInput} = useTexts()
@@ -99,7 +100,7 @@ function Selects({
     <div className={`flex ${matches ? "" : "flex-col"} gap-1 px-5`}>
       <FormControl variant="standard" sx={{ width: 150 }}>
         <InputLabel>
-          {languages === 'ko' ? '위치가 어디인가요' : 'Which location?'}
+          {language === 'ko' ? '위치가 어디인가요' : 'Which location?'}
         </InputLabel>
         <Select
           value={locationState.locationOne}
@@ -108,9 +109,9 @@ function Selects({
         >
           {locationsBuildingsArray.map((value, index) => {
             const key = Object.keys(value)[0]
-            const campuses = languages === 'ko' ? ['서울', '국제', '광릉'] : ['Seoul', 'Global', 'Gwangneung']
+            const campuses = language === 'ko' ? ['서울', '국제', '광릉'] : ['Seoul', 'Global', 'Gwangneung']
             const campusIndex = key.slice(0, 2) === 'se' ? 0 : key.slice(0, 2) === 'gu' ? 1 : 2
-            const nameWithoutCampus = campuses.indexOf(value[key][languages].name.split(' ')[0]) === -1 ? value[key][languages].name : value[key][languages].name.replace(value[key][languages].name.split(' ')[0], '')
+            const nameWithoutCampus = campuses.indexOf(value[key][language].name.split(' ')[0]) === -1 ? value[key][language].name : value[key][language].name.replace(value[key][language].name.split(' ')[0], '')
             const name = campuses[campusIndex] + ': ' + nameWithoutCampus
             const koBuilding = value[key].ko.name
             if (key.slice(2) === 'all') return null
@@ -127,7 +128,7 @@ function Selects({
             <InputLabel
             // id="demo-simple-select-standard-label1"
             >
-              {languages === 'en' && 'Where in '}{languages === 'ko' ? locationState.locationOne : locationsBuildings['en'][locationsBuildings['ko'].indexOf(locationState.locationOne)]}{languages === 'ko' && ' 어디인가요'}
+              {language === 'en' && 'Where in '}{language === 'ko' ? locationState.locationOne : locationsBuildings['en'][locationsBuildings['ko'].indexOf(locationState.locationOne)]}{language === 'ko' && ' 어디인가요'}
             </InputLabel>
             <Select
               // labelId="demo-simple-select-standard-label1"
@@ -137,15 +138,15 @@ function Selects({
             // label="Age"
             >
               {menuItems}
-              {/* {locationState.locationOne === "중도" && location.cl[languages]}
-              {locationState.locationOne === "청운" && location.cw[languages]}
-              {locationState.locationOne === "푸른솔" && location.p[languages]}
-              {locationState.locationOne === "간호이과대" && location.g[languages]}
-              {locationState.locationOne === "네오르네상스관" && location.n[languages]}
-              {locationState.locationOne === "경영대" && location.k[languages]}
-              {locationState.locationOne === "문과대" && location.m[languages]}
-              {locationState.locationOne === "의과대" && location.e[languages]}
-              {locationState.locationOne === "치과병원" && location.c[languages]} */}
+              {/* {locationState.locationOne === "중도" && location.cl[language]}
+              {locationState.locationOne === "청운" && location.cw[language]}
+              {locationState.locationOne === "푸른솔" && location.p[language]}
+              {locationState.locationOne === "간호이과대" && location.g[language]}
+              {locationState.locationOne === "네오르네상스관" && location.n[language]}
+              {locationState.locationOne === "경영대" && location.k[language]}
+              {locationState.locationOne === "문과대" && location.m[language]}
+              {locationState.locationOne === "의과대" && location.e[language]}
+              {locationState.locationOne === "치과병원" && location.c[language]} */}
             </Select>
           </FormControl>
         )}
@@ -156,7 +157,7 @@ function Selects({
             <InputLabel
             // id="demo-simple-select-standard-label1"
             >
-              {languages === 'ko' ? '좌석이 어디인가요' : 'Where is your seat'}
+              {language === 'ko' ? '좌석이 어디인가요' : 'Where is your seat'}
             </InputLabel>
             <Select
               // labelId="demo-simple-select-standard-label1"
