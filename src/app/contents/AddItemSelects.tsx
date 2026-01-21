6879ad0d8336d2859c422ee86ba4211a1249a838
usany <ckd_qja@naver.com>
2026-01-21 21:06:57 +0900

    cern

--- src/app/contents/AddItemSelects.tsx
+++ src/app/contents/AddItemSelects.tsx
@@ -1,27 +1,27 @@
+import { useLanguage } from '@/context/LanguageContext';
 import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
-import useSelectors from 'src/hooks/useSelectors';
 
 interface Props {
   item: string
   changeItem: () => void
 }
 function AddItemSelects({ item, changeItem }: Props) {
-  const languages = useSelectors((state) => state.languages.value)
+  const { language } = useLanguage()
 
   return (
     <div>
       <FormControl variant="standard" sx={{ minWidth: 150 }}>
         <InputLabel
-        >{languages === 'ko' ? '우산 / 양산 선택' : 'Select Usan / Parasol'}</InputLabel>
+        >{language === 'ko' ? '우산 / 양산 선택' : 'Select Usan / Parasol'}</InputLabel>
         <Select
           value={item}
           onChange={changeItem}
         >
-          <MenuItem value={'우산'}>{languages === 'ko' ? '우산' : 'Umbrella'}</MenuItem>
-          <MenuItem value={'양산'}>{languages === 'ko' ? '양산' : 'Parasol'}</MenuItem>
+          <MenuItem value={'우산'}>{language === 'ko' ? '우산' : 'Umbrella'}</MenuItem>
+          <MenuItem value={'양산'}>{language === 'ko' ? '양산' : 'Parasol'}</MenuItem>
         </Select>
       </FormControl>
     </div>
