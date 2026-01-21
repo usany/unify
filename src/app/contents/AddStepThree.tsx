8017cc8897e8a4a3106d8cabbab92fa0025e29ad
usany <ckd_qja@naver.com>
2026-01-21 21:21:34 +0900

    cern

--- src/app/contents/AddStepThree.tsx
+++ src/app/contents/AddStepThree.tsx
@@ -1,4 +1,4 @@
-import useSelectors from 'src/hooks/useSelectors'
+import { useLanguage } from '@/context/LanguageContext'
 import AddStepTitle from 'src/pages/add/AddStepTitle'
 import Pickers from 'src/pages/add/Pickers'
 
@@ -8,17 +8,14 @@ interface Props {
 }
 
 const AddStepThree = ({ onChangeFrom, onChangeTo }: Props) => {
-  const languages = useSelectors((state) => state.languages.value)
-
-  // const title = ['3. 시간 입력']
+  const { language } = useLanguage()
   const titles = {
     ko: '3. 시간 입력',
     en: '3. Input time'
   }
-  const index = (languages === 'ko' || languages === 'en') ? languages : 'ko'
   return (
     <div>
-      <AddStepTitle title={titles[index]} />
+      <AddStepTitle title={titles[language]} />
       <div className='flex flex-col px-5'>
         <Pickers onChange={onChangeFrom} label={`${languages === 'ko' ? '이 때부터' : 'From'}`} />
         <Pickers onChange={onChangeTo} label={`${languages === 'ko' ? '이 때까지' : 'To'}`} />
