3365b421a805378f84654563616d6f05acd3c4bf
usany <ckd_qja@naver.com>
2026-01-21 21:04:13 +0900

    cern

--- src/app/contents/AddCards.tsx
+++ src/app/contents/AddCards.tsx
@@ -30,6 +30,12 @@ interface Props {
   locationState: object
 }
 const AddCards = ({ borrow, item, fromTo, locationState }: Props) => {
+  const profile = {
+    profileImage: true,
+    defaultProfile: 'https://ijsfbngiyhgvolsprxeh.supabase.co/storage/v1/object/public/remake/animalprofileRed.png',
+    profileImageUrl: 'https://ijsfbngiyhgvolsprxeh.supabase.co/storage/v1/object/public/remake/animalprofileRed.png',
+
+  }
   const shadowColorArray = [
     'lightblue',
     'lightcoral',
