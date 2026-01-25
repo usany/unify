a0c5e5f9e55cd263d90057d7696ddb04c5fd538b
usany <ckd_qja@naver.com>
2026-01-25 21:16:18 +0900

    cern

--- src/app/contents/Playlist.tsx
+++ src/app/contents/Playlist.tsx
@@ -3,13 +3,15 @@ import { useTheme } from '@/app/context/ThemeContext';
 const Playlist = () => {
   const { theme } = useTheme();
   return (
-    <iframe
-      src={'https://open.spotify.com/embed/playlist/41clCj2piQBL3BSEFQN9J3?utm_source=generator' + `${theme !== 'light' ? '&theme=0' : ''}`}
-      width="90%"
-      height="200"
-      allow="autoplay; clipboard-write; fullscreen; picture-in-picture"
-      loading="lazy"
-    />
+    <div className='flex justify-center'>
+      <iframe
+        src={'https://open.spotify.com/embed/playlist/41clCj2piQBL3BSEFQN9J3?utm_source=generator' + `${theme !== 'light' ? '&theme=0' : ''}`}
+        width="315px"
+        height="152px"
+        allow="autoplay; clipboard-write; fullscreen; picture-in-picture"
+        loading="lazy"
+      />
+    </div>
   )
 }
 
