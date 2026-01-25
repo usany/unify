f6bdb9a6c6e700017679dc1ab93c59efb73ca6e0
usany <ckd_qja@naver.com>
2026-01-25 20:50:38 +0900

    cern

--- src/components/MuiThemeProvider.tsx
+++ src/components/MuiThemeProvider.tsx
@@ -66,7 +66,7 @@ export default function MuiThemeProvider({ children }: MuiThemeProviderProps) {
       MuiButton: {
         defaultProps: {
           sx: {
-            backgroundColor: theme === 'dark' ? '#37474f' : '#f0fbe7',
+            backgroundColor: theme === 'dark' ? '#37474f' : '#f0fbd7',
             color: theme === 'dark' ? '#ffffff' : '#000000',
             ':hover': {
               backgroundColor: theme === 'dark' ? '#304000' : '#f0f4c3',
