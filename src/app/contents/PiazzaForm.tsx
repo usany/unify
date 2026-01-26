f0fb0b47ad5443234da6164631983411db1d2d42
charlie <ckd_qja@naver.com>
2026-01-27 05:34:53 +0900

    cern

--- src/app/contents/PiazzaForm.tsx
+++ src/app/contents/PiazzaForm.tsx
@@ -3,15 +3,7 @@ import { useLanguage } from '@/context/LanguageContext'
 import { useState } from 'react'
 
 function PiazzaForm({ addMessage }: { addMessage: (event: React.FormEvent<HTMLFormElement>) => void }) {
-  const [message, setMessage] = useState({
-    id: 'user',
-    msg: '',
-    userUid: '2',
-    messageClock: new Date(),
-    defaultProfile: 'https://ijsfbngiyhgvolsprxeh.supabase.co/storage/v1/object/public/remake/animalprofileBlue.png',
-    profileImageUrl: 'https://ijsfbngiyhgvolsprxeh.supabase.co/storage/v1/object/public/remake/animalprofileBlue.png',
-    profileImage: true,
-  })
+  const [message, setMessage] = useState('')
   const { language } = useLanguage()
   return (
     <form id='piazza' className='flex' onSubmit={addMessage}>
@@ -22,10 +14,10 @@ function PiazzaForm({ addMessage }: { addMessage: (event: React.FormEvent<HTMLFo
         type="text"
         name="piazza"
         className="w-full p-3 rounded bg-light-1 dark:bg-dark-1"
-        value={message.msg}
-        onChange={(event) => setMessage({ ...message, msg: event.target.value })}
+        value={message}
+        onChange={(event) => setMessage('')}
       />
-      <button className="w-1/6 rounded bg-light-2 dark:bg-dark-2" type="submit" onClick={() => setMessage({ ...message, msg: '' })}>
+      <button className="w-1/6 rounded bg-light-2 dark:bg-dark-2" type="submit" onClick={() => setMessage('')}>
         {language === 'en' ? 'Send' : '전송'}
       </button>
     </form>
