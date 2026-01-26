450ada09af644235c7d463f5f142dc8919b41cd3
charlie <ckd_qja@naver.com>
2026-01-27 05:29:05 +0900

    cern

--- src/app/contents/PiazzaForm.tsx
+++ src/app/contents/PiazzaForm.tsx
@@ -4,14 +4,14 @@ import { useState } from 'react'
 
 function PiazzaForm({ addMessage }: { addMessage: (event: React.FormEvent<HTMLFormElement>) => void }) {
   const [message, setMessage] = useState({
-      id: 'KHUSAN',
-      msg: 'Welcome to KHUSAN',
-      userUid: '1',
-      messageClock: new Date(),
-      defaultProfile: 'https://ijsfbngiyhgvolsprxeh.supabase.co/storage/v1/object/public/remake/animalprofileRed.png',
-      profileImageUrl: 'https://ijsfbngiyhgvolsprxeh.supabase.co/storage/v1/object/public/remake/animalprofileRed.png',
-      profileImage: true,
-    })
+    id: 'user',
+    msg: '',
+    userUid: '2',
+    messageClock: new Date(),
+    defaultProfile: 'https://ijsfbngiyhgvolsprxeh.supabase.co/storage/v1/object/public/remake/animalprofileRed.png',
+    profileImageUrl: 'https://ijsfbngiyhgvolsprxeh.supabase.co/storage/v1/object/public/remake/animalprofileRed.png',
+    profileImage: true,
+  })
   const { language } = useLanguage()
   return (
     <form id='piazza' className='flex' onSubmit={addMessage}>
