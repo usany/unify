274b4eef5014f848a7fbdae2b6961b035d2ae488
charlie <ckd_qja@naver.com>
2026-01-27 05:25:07 +0900

    cern

--- src/app/contents/PiazzaForm.tsx
+++ src/app/contents/PiazzaForm.tsx
@@ -1,5 +1,6 @@
 import { AlarmCheck, PlusCircle, UserRound } from 'lucide-react'
 import { useLanguage } from '@/context/LanguageContext'
+import { useState } from 'react'
 
 function PiazzaForm({ addMessage }: { addMessage: (event: React.FormEvent<HTMLFormElement>) => void }) {
   const [message, setMessage] = useState('')
@@ -14,7 +15,7 @@ function PiazzaForm({ addMessage }: { addMessage: (event: React.FormEvent<HTMLFo
         name="piazza"
         className="w-full p-3 rounded bg-light-1 dark:bg-dark-1"
       />
-      <button className="w-1/6 rounded bg-light-2 dark:bg-dark-2" type="submit" onClick={}>
+      <button className="w-1/6 rounded bg-light-2 dark:bg-dark-2" type="submit" onClick={() => setMessage('')}>
         {language === 'en' ? 'Send' : '전송'}
       </button>
     </form>
