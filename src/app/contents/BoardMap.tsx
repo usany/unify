0c0cf4d867d32b7390fe767d8cb2cba5eb28e6c7
charlie <ckd_qja@naver.com>
2026-01-28 05:56:45 +0900

    cern

--- src/app/contents/BoardMap.tsx
+++ src/app/contents/BoardMap.tsx
@@ -91,7 +91,7 @@ function BoardMap() {
             </div>
             <div key={index} className="flex gap-5">
                 <div className="pt-1">
-                  ${language === 'en' ? 'itemOne' : '물건 1'}
+                  ${language === 'en' ? 'Umbrella' : '우산'}
                 </div>
                 <div className="pt-3">
                   ${language === 'en' ? 'borrowing' : '빌리기'}
@@ -102,7 +102,7 @@ function BoardMap() {
                   ${0}
                 </div>
                 <div className="pt-1">
-                  ${language === 'en' ? 'itemTwo' : '물건 2'}
+                  ${language === 'en' ? 'Parasol' : '양산'}
                 </div>
                 <div className="pt-3">
                   ${language === 'en' ? 'borrowing' : '빌리기'}
