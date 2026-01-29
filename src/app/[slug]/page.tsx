4a42d7b43725dd32326af57653b6e2d0da7bc27f
charlie <ckd_qja@naver.com>
2026-01-29 21:44:28 +0900

    cern

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,9 +1,6 @@
 
 import { redirect } from 'next/navigation';
 import SlugLayout from './components/SlugLayout';
-import MDXContent from './components/MDXContent';
-import dynamic from 'next/dynamic';
-import { cookies } from 'next/headers';
 
 export default async function Page({
   params,
@@ -15,9 +12,6 @@ export default async function Page({
   if (!possibleLinks.includes(slug)) {
     redirect('/')
   }
-
-  const cookieStore = await cookies()
-  const language = cookieStore.get('language')?.value || 'ko'
 
   return (
     <SlugLayout pageId={slug} />
