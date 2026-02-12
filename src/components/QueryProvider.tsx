e4b59a93cc37e62f1e380a2ddccf6bc4920844c2
charlie <ckd_qja@naver.com>
2026-02-13 04:10:54 +0900

    cern

--- src/components/QueryProvider.tsx
+++ src/components/QueryProvider.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { useState } from 'react';
 
 export default function QueryProvider({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
