29d24e9b34c3ce04d04726490fc95e660faa7613
charlie <ckd_qja@naver.com>
2026-02-19 21:51:43 +0900

    cern

--- src/app/contents/PiazzaScreen.tsx
+++ src/app/contents/PiazzaScreen.tsx
@@ -30,15 +30,7 @@ function PiazzaScreen() {
       setMessages([...messages, message])
     }
   }
-  // const messagesArray = [{
-  //   id: 'KHUSAN',
-  //   msg: 'Welcome to KHUSAN',
-  //   userUid: '1',
-  //   messageClock: new Date(),
-  //   defaultProfile: 'https://ijsfbngiyhgvolsprxeh.supabase.co/storage/v1/object/public/remake/animalprofileRed.png',
-  //   profileImageUrl: 'https://ijsfbngiyhgvolsprxeh.supabase.co/storage/v1/object/public/remake/animalprofileRed.png',
-  //   profileImage: true,
-  // }]
+
   return (
     <>
       <br />
