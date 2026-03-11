e26f3bd473a94b3a2a6523971dae97644cbb9eea
usany <ckd_qja@naver.com>
2026-03-11 19:41:37 +0900

    fix

--- app/routes/process.tsx
+++ app/routes/process.tsx
@@ -166,6 +166,20 @@ export default function Process() {
                   const currentHour = new Date().getHours();
                   const currentMinute = new Date().getMinutes();
                   const isNightTime = currentHour > 18 || currentHour < 6;
+                  let nextBus = 4
+                  if (currentHour >= 6 && currentHour <= 8) {
+                    if (currentHour === 8 && currentMinute >= 45) {
+                      nextBus = 1
+                    } else {
+                      nextBus = 0
+                    }
+                  } else if (currentHour > 8 && currentHour < 10) {
+                    nextBus = 1
+                  } else if (currentHour >= 10) {
+                    nextBus = 2
+                  } else if (currentHour < 18) {
+                    nextBus = 3
+                  }
                   return (
                     <div key={index} className="flex items-center space-x-6">
                       <div className={`w-18 h-16 ${isNightTime ? 'bg-green-600' : 'bg-blue-600'} text-white rounded-md flex items-center justify-center font-semibold text-md z-10`}>
