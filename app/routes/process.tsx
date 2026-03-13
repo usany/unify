e02d9ca47643e21c8ef315074d0cdf74bf361a69
usany <ckd_qja@naver.com>
2026-03-13 20:39:43 +0900

    fix

--- app/routes/process.tsx
+++ app/routes/process.tsx
@@ -7,7 +7,9 @@ export const process = {
   busFrom: '사색의 광장-정문 건너편',
   shuttleSeoul: '서울-국제 셔틀버스',
   shuttleGlobal: '국제-서울 셔틀버스',
-  commute: '영통역 통학버스'
+  commute: '영통역 통학버스',
+  busGwangnengOne: '봉선사입구-내산정 방면',
+  busGwangnengTwo: '내산정-봉선사입구 방면',
 } as { [key: string]: string };
 export const commuteTime = [
   "8:45am",
